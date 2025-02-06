61fa4bcfe4b5e17ab91cc89bb7be11347cd85966
thejaniperera <nawodathejani1999@gmail.com>
2025-02-06 07:29:27 -0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup 
+
+setup( 
+    name='My AI Project', 
+    version='1.0.0', 
+    author='Thejani',  # You can use your own name
+    author_email='nawodathejani1999@gmail.com',  # Use any valid email
+    packages=find_packages(), 
+    install_requires=[]  
+)
